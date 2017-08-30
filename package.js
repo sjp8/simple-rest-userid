4d78771a535cc940f17330ba260569be898eb6e9
John-Paul Simonis <sjp8@users.noreply.github.com>
2017-08-30 01:43:10 -0700

    initial commit

---
+++ package.js
@@ -0,0 +1,61 @@
+Package.describe({
+  name: 'sjp8:simple-rest-userid',
+  version: '1.0.0',
+
+  // Brief, one-line summary of the package.
+  summary: 'simple:rest with support for Meteor.userId() and Meteor.user() in methods endpoints.',
+
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/sjp8/simple-rest-userid/blob/master/',
+
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md',
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use([
+    'check',
+    'ddp',
+    'ejson',
+    'meteor',
+    'mongo',
+    'simple:json-routes@2.1.0',
+    'underscore',
+    'webapp',
+  ], 'server');
+
+  api.use([
+    'accounts-base',
+  ], 'server', {weak: true});
+
+  api.addFiles([
+    'http-connection.js',
+    'http-subscription.js',
+    'rest.js',
+    'list-api.js',
+  ], 'server');
+
+  api.export('SimpleRest');
+});
+
+Package.onTest(function (api) {
+  api.use([
+    'check',
+    'http',
+    'jquery',
+    'mongo',
+    'random',
+    'simple:json-routes',
+    'sjp8:simple-rest-userid',
+    'simple:rest-accounts-password',
+    'simple:rest-json-error-handler',
+    'test-helpers',
+    'tinytest',
+    'underscore',
+  ]);
+
+  api.addFiles('rest-tests.js');
+});
